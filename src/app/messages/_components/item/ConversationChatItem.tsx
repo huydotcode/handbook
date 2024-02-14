3f86c0baae4f70a301549ee0b73eae00100ff16a
huydotcode <ngonhuthuy1234@gmail.com>
2024-02-14 12:08:19 +0700

    new commit

--- src/app/messages/_components/item/ConversationChatItem.tsx
+++ src/app/messages/_components/item/ConversationChatItem.tsx
@@ -1,8 +1,9 @@
 'use client';
-import { Avatar, Button } from '@/components/ui';
+import { Button, Icons } from '@/components/ui';
 import { useChat } from '@/context/ChatContext';
 
 import { useSession } from 'next-auth/react';
+import Image from 'next/image';
 import React from 'react';
 
 interface Props {
@@ -21,15 +22,22 @@ const ConversationChatItem: React.FC<Props> = ({ data: conversation }) => {
         <>
             <Button
                 className={`flex h-[60px] w-full cursor-pointer items-center px-4 py-2 ${
-                    isSelect && ''
+                    isSelect && 'bg-primary-1'
                 }`}
                 key={otherUserId}
                 href={`/messages/r/${otherUserId}`}
                 variant={'custom'}
             >
-                <Avatar imgSrc={conversation.image} />
+                <Image
+                    className="rounded-full"
+                    priority={true}
+                    src={conversation.image}
+                    alt={conversation.name}
+                    width={32}
+                    height={32}
+                />
 
-                <div className="flex flex-1 flex-col">
+                <div className="flex flex-1 flex-col md:hidden">
                     <div className="flex items-center justify-between">
                         <h3 className="ml-2 whitespace-nowrap text-sm font-bold">
                             {conversation.name}
