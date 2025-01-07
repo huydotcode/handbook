6318872b81d11dddb2f46fd8a932111a2f8d449a
huydotcode <ngonhuthuy1234@gmail.com>
2025-01-07 08:39:37 +0700

    new commmit

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,10 +4,6 @@ export { default as DarkmodeButton } from './DarkmodeButton';
 export { default as Icons } from './Icons';
 export { default as Loading } from './Loading';
 export { default as Modal } from './Modal';
-export { default as Popover } from './Popover';
 export { default as SlideShow } from './SlideShow';
 export { default as TextEditor } from './TextEditor';
 export { default as Collapse } from './Collapse';
-
-// Skeletons
-export { default as CommentSkeleton } from './skeleton/CommentSkeleton';
