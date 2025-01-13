c59b5642f2fa43e5e9a68e2b3e06e5b2af11d0bc
huydotcode <ngonhuthuy1234@gmail.com>
2025-01-13 22:01:26 +0700

    update axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.SERVER_API || 'http://localhost:8000/api/v1';
+let BASE_URL;
+
+if (process.env.NODE_ENV === 'development') {
+    BASE_URL = 'http://localhost:8000/api/v1';
+} else {
+    BASE_URL = 'https://handbook-api.vercel.app/api/v1';
+}
 
 console.log('BASE_URL', BASE_URL);
 
