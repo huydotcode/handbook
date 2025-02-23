ac970ac6a34a2cacb0736a28ccc36c60e597a9e2
huydotcode <ngonhuthuy1234@gmail.com>
2025-02-23 07:26:10 +0700

    update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -78,6 +78,8 @@ export const authOptions: NextAuthOptions = {
                     process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                 // 30s
                 expires: new Date(Date.now() + 30 * 1000),
+                domain:
+                    process.env.NODE_ENV === 'production' ? '.vercel.app' : '',
             },
         },
     },
