e1004079f85b78add1a7183ae412ef838897fa4d
huydotcode <ngonhuthuy1234@gmail.com>
2025-03-11 15:48:59 +0700

    update mongo

--- src/services/mongodb.ts
+++ src/services/mongodb.ts
@@ -1,5 +1,18 @@
 import mongoose from 'mongoose';
 import { config } from '../utils/config';
+import Conversation from '../models/conversation.model';
+import ConversationRole from '../models/conversationrole.model';
+import Follows from '../models/follow.model';
+import Group from '../models/group.model';
+import Image from '../models/image.model';
+import Item from '../models/item.model';
+import Location from '../models/location.model';
+import Message from '../models/message.model';
+import Notification from '../models/notification.model';
+import Post from '../models/post.model';
+import Profile from '../models/profile.model';
+import SavedPost from '../models/savedpost.model';
+import User from '../models/user.model';
 
 let isConnected = false;
 
@@ -16,6 +29,20 @@ export const connectToMongo = async () => {
             autoIndex: true,
         });
         console.log('Connected to MongoDB');
+
+        Conversation.createCollection();
+        ConversationRole.createCollection();
+        Follows.createCollection();
+        Group.createCollection();
+        Image.createCollection();
+        Item.createCollection();
+        Location.createCollection();
+        Message.createCollection();
+        Notification.createCollection();
+        Post.createCollection();
+        Profile.createCollection();
+        SavedPost.createCollection();
+        User.createCollection();
 
         isConnected = true;
     } catch (error: any) {
