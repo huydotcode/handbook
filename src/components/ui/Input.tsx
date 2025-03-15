f1ce4396f85c01b7ac4b7a93c2a82204202b0e49
huydotcode <ngonhuthuy1234@gmail.com>
2025-03-15 22:19:46 +0700

    update ui form

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,14 +4,17 @@ import { cn } from '@/lib/utils';
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
     ({ className, type, ...props }, ref) => {
+        if (typeof window === 'undefined') return null;
+
         return (
             <input
                 type={type}
                 className={cn(
-                    'flex h-10 w-full rounded-md border-none bg-background px-3 py-2 text-base text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+                    'flex h-10 w-full rounded-md border-none bg-primary-1 px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:bg-dark-primary-1  md:text-sm',
                     className
                 )}
                 ref={ref}
+                suppressHydrationWarning
                 {...props}
             />
         );
