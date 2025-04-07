fcecc32a2b980f98f1e6662e91358cb1924ca515
huydotcode <ngonhuthuy1234@gmail.com>
2025-04-07 14:07:09 +0700

    update skeleton comment

--- src/components/post/comment/SkeletonComment.tsx
+++ src/components/post/comment/SkeletonComment.tsx
@@ -4,7 +4,7 @@ const SkeletonComment = () => {
             <div className="h-8 w-8 animate-skeleton rounded-full bg-skeleton dark:bg-dark-secondary-2"></div>
             <div className="flex-1 space-y-2">
                 <div className="h-10 w-3/4 animate-skeleton rounded-xl bg-skeleton dark:bg-dark-secondary-2"></div>
-                <div className="h-4 w-3/4 animate-skeleton rounded-xl bg-skeleton dark:bg-dark-secondary-2"></div>
+                <div className="h-4 w-2/5 animate-skeleton rounded-xl bg-skeleton dark:bg-dark-secondary-2"></div>
             </div>
         </div>
     );
