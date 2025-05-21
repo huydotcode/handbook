fc074f70b425bd9a267993ba7af86b07f2d74cdc
huydotcode <ngonhuthuy1234@gmail.com>
2025-05-21 18:24:05 +0700

    update saved-post page

--- src/app/(admin)/admin/market/page.tsx
+++ src/app/(admin)/admin/market/page.tsx
@@ -25,140 +25,150 @@ interface CategoryForm {
 }
 
 const CategoryPage: React.FC = () => {
-    // const { data, refetch } = useQuery({
-    //     queryKey: getCategoriesKey(),
-    //     queryFn: async () => {
-    //         return await getCategories();
-    //     },
-    // });
-    //
-    // const form = useForm<CategoryForm>({
-    //     defaultValues: {
-    //         name: '',
-    //         description: '',
-    //         slug: '',
-    //         icon: '',
-    //     },
-    // });
-    // const { handleSubmit, register, formState } = form;
-    //
-    // const onSubmit = async (data: CategoryForm) => {
-    //     try {
-    //         const newCategory = await createCategory(data);
-    //
-    //         await refetch();
-    //
-    //         toast.success('Category created successfully');
-    //     } catch (error) {
-    //         console.error(error);
-    //         toast.error('Failed to create category');
-    //     }
-    // };
+    const { data, refetch } = useQuery({
+        queryKey: getCategoriesKey(),
+        queryFn: async () => {
+            return await getCategories();
+        },
+    });
 
-    // return (
-    //     <div className={'h-screen w-full bg-secondary-1'}>
-    //         <h1 className="text-xl font-bold">Categories</h1>
-    //         <div className="flex items-center p-2">
-    //             {data?.map((category: any) => {
-    //                 const Icon = IconsArray.find(
-    //                     (icon) => icon.name === category.icon
-    //                 )?.icon;
-    //
-    //                 return (
-    //                     <div
-    //                         className={
-    //                             'flex items-center rounded border border-gray-300 p-2'
-    //                         }
-    //                         key={category._id}
-    //                     >
-    //                         {Icon && <Icon className="mr-2" />}
-    //                         <span>{category.name}</span>
-    //                     </div>
-    //                 );
-    //             })}
-    //         </div>
-    //
-    //         <Form {...form}>
-    //             <form
-    //                 className={
-    //                     'mx-auto my-2 max-w-[500px] rounded-xl border p-4'
-    //                 }
-    //                 onSubmit={handleSubmit(onSubmit)}
-    //             >
-    //                 <h1 className="text-xl font-bold">Create Category</h1>
-    //
-    //                 <FormField
-    //                     control={form.control}
-    //                     name="name"
-    //                     render={({ field }) => (
-    //                         <FormItem>
-    //                             <FormLabel>Name</FormLabel>
-    //                             <FormControl>
-    //                                 <Input placeholder="Name" {...field} />
-    //                             </FormControl>
-    //                             <FormMessage />
-    //                         </FormItem>
-    //                     )}
-    //                 />
-    //
-    //                 <FormField
-    //                     control={form.control}
-    //                     name="description"
-    //                     render={({ field }) => (
-    //                         <FormItem>
-    //                             <FormLabel>Description</FormLabel>
-    //                             <FormControl>
-    //                                 <Input
-    //                                     placeholder="Description"
-    //                                     {...field}
-    //                                 />
-    //                             </FormControl>
-    //                             <FormMessage />
-    //                         </FormItem>
-    //                     )}
-    //                 />
-    //
-    //                 <FormField
-    //                     control={form.control}
-    //                     name="slug"
-    //                     render={({ field }) => (
-    //                         <FormItem>
-    //                             <FormLabel>Slug</FormLabel>
-    //                             <FormControl>
-    //                                 <Input placeholder="Slug" {...field} />
-    //                             </FormControl>
-    //                             <FormMessage />
-    //                         </FormItem>
-    //                     )}
-    //                 />
-    //
-    //                 <FormField
-    //                     control={form.control}
-    //                     name="icon"
-    //                     render={({ field }) => (
-    //                         <FormItem>
-    //                             <FormLabel>Icon</FormLabel>
-    //                             <FormControl>
-    //                                 <Input placeholder="Icon" {...field} />
-    //                             </FormControl>
-    //                             <FormMessage />
-    //                         </FormItem>
-    //                     )}
-    //                 />
-    //
-    //                 <Button
-    //                     className={'mx-auto mt-2'}
-    //                     type="submit"
-    //                     disabled={formState.isSubmitting}
-    //                 >
-    //                     Submit
-    //                 </Button>
-    //             </form>
-    //         </Form>
-    //     </div>
-    // );
+    const form = useForm<CategoryForm>({
+        defaultValues: {
+            name: '',
+            description: '',
+            slug: '',
+            icon: '',
+        },
+    });
+    const { handleSubmit, register, formState } = form;
 
-    return <></>;
+    const onSubmit = async (data: CategoryForm) => {
+        try {
+            const newCategory = await createCategory(data);
+
+            await refetch();
+
+            toast.success('Category created successfully');
+        } catch (error) {
+            console.error(error);
+            toast.error('Failed to create category');
+        }
+    };
+
+    return (
+        <div className={'h-screen w-full bg-secondary-1'}>
+            <h1 className="text-xl font-bold">Categories</h1>
+            <div className="flex items-center p-2">
+                {data?.map((category: any) => {
+                    const Icon = IconsArray.find(
+                        (icon) => icon.name === category.icon
+                    )?.icon;
+
+                    return (
+                        <div
+                            className={
+                                'flex items-center rounded border border-gray-300 p-2'
+                            }
+                            key={category._id}
+                        >
+                            {Icon && <Icon className="mr-2" />}
+                            <span>{category.name}</span>
+                        </div>
+                    );
+                })}
+            </div>
+
+            <Form {...form}>
+                <form
+                    className={
+                        'mx-auto my-2 max-w-[500px] rounded-xl border p-4'
+                    }
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <h1 className="text-xl font-bold">Create Category</h1>
+
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Name" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Description</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Description"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="slug"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Slug</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Slug" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <label htmlFor="">Icon</label>
+                    <select
+                        {...register('icon')}
+                        className="w-full rounded border p-2"
+                        defaultValue={''}
+                    >
+                        {IconsArray.map((icon) => (
+                            <option key={icon.name} value={icon.name}>
+                                {icon.name}
+                            </option>
+                        ))}
+                    </select>
+                    {/* <FormField
+                        control={form.control}
+                        name="icon"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Icon</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Icon" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    /> */}
+
+                    <Button
+                        className={'mx-auto mt-2'}
+                        type="submit"
+                        disabled={formState.isSubmitting}
+                    >
+                        Submit
+                    </Button>
+                </form>
+            </Form>
+        </div>
+    );
 };
 
 export default CategoryPage;
--- src/app/(routes)/saved/page.tsx
+++ src/app/(routes)/saved/page.tsx
@@ -1,3 +1,4 @@
+import { FriendSection, Sidebar } from '@/components/layout';
 import { Post } from '@/components/post';
 import { getSavedPosts } from '@/lib/actions/post.action';
 import { getAuthSession } from '@/lib/auth';
@@ -12,18 +13,24 @@ export default async function SavedPage() {
     })) as ISavedPost;
 
     return (
-        <div className="mx-auto mt-[64px] w-[800px] max-w-screen">
-            <h1 className="text-2xl font-bold">Đã lưu</h1>
+        <div className="relative top-[56px] mx-auto min-h-[calc(100vh-56px)] w-[1200px] max-w-screen md:w-screen">
+            <Sidebar />
 
-            <div className="mt-4">
-                {postsSaved.posts.reverse().map((post) => (
-                    <Post key={post._id} data={post} isSaved={true} />
-                ))}
+            <div className="mx-auto mt-2 w-[600px] xl:w-[550px] md:w-full">
+                <h1 className="text-2xl font-bold">Đã lưu</h1>
 
-                {postsSaved.posts.length === 0 && (
-                    <p className="text-center">Không có bài viết nào</p>
-                )}
+                <div className="mt-4">
+                    {postsSaved.posts.reverse().map((post) => (
+                        <Post key={post._id} data={post} isSaved={true} />
+                    ))}
+
+                    {postsSaved.posts.length === 0 && (
+                        <p className="text-center">Không có bài viết nào</p>
+                    )}
+                </div>
             </div>
+
+            {session && <FriendSection session={session} />}
         </div>
     );
 }
