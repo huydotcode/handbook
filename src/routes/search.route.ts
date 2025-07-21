0be71755f8a5313db18c67e6625a95a1ecb43b27
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2025-07-21 22:55:49 +0700

    update search route, controller

--- src/routes/search.route.ts
+++ src/routes/search.route.ts
@@ -3,6 +3,9 @@ import SearchController from '../controllers/search.controller';
 
 const searchRouter = Router();
 
-searchRouter.get('/', SearchController.search)
+searchRouter.get('/', SearchController.search);
+searchRouter.get('/users', SearchController.searchUsers);
+searchRouter.get('/posts', SearchController.searchPosts);
+searchRouter.get('/groups', SearchController.searchGroups);
 
 export default searchRouter;
